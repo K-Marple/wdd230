434631fb4af2b74bdb9062a92f2cacf8b39eaf7e
K-Marple <taeshinoberu@outlook.com>
2024-05-21 14:19:01 -0700

    steps for group work added to bom.js

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,24 @@
+// Declare 3 const variables that reference input, button, and list
+
+// Create function to run when Add Chapter button is clicked
+
+    // Check that the input is not blank with if block, otherwise provide 
+    // a message or do nothing and focus on input
+
+    // Create a li element
+
+    // Create a delete button
+
+    // Populate li elements textContent or innerHTML with input value
+
+    // Populate button textContent with an x
+
+    // Append li element to unordered list in HTML
+
+    // Create function to delete li element when delete button is clicked
+
+    // Send focus to input element
+
+    // Change input to blank to clean up the interface for user
+
+    
