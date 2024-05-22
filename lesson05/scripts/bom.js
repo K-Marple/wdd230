c83fb1960623cc2baffc4e587c4494661f562e39
K-Marple <taeshinoberu@outlook.com>
2024-05-22 08:45:11 -0700

    bom javascript added

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -1,24 +1,35 @@
 // Declare 3 const variables that reference input, button, and list
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const list = document.querySelector("ul");
 
 // Create function to run when Add Chapter button is clicked
+button.addEventListener("click", () => {
+    // Check that the input is not blank with if block, otherwise provide a message or do nothing and focus on input
+    if (input.value != "") {
+        // Create a li element and  a delete button, populate li and button textContent
+        const listItem = document.createElement("li");
+        const deleteBtn = document.createElement("button");
+        listItem.textContent = input.value;
+        deleteBtn.textContent = "❌";
 
-    // Check that the input is not blank with if block, otherwise provide 
-    // a message or do nothing and focus on input
+        // Append li element to unordered list in HTML and append delete button to li element
+        list.appendChild(listItem);
+        listItem.appendChild(deleteBtn);
 
-    // Create a li element
+        // Create function to delete li element when delete button is clicked
+        deleteBtn.addEventListener("click", () => {
+            list.removeChild(listItem);
+        });
 
-    // Create a delete button
+        // Send focus to input element
+        input.focus();
 
-    // Populate li elements textContent or innerHTML with input value
+        // Change input to blank to clean up the interface for user
+        input.value = "";
+    }
 
-    // Populate button textContent with an x
-
-    // Append li element to unordered list in HTML
-
-    // Create function to delete li element when delete button is clicked
-
-    // Send focus to input element
-
-    // Change input to blank to clean up the interface for user
-
-    
+    else {
+        input.focus();
+    }
+});
