110d8a1e1fcca43ca7b6df294d3814c2b37cc1ec
K-Marple <taeshinoberu@outlook.com>
2024-05-31 20:26:34 -0700

    dark mode changes

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -1,7 +1,9 @@
 // dark mode
+const root = document.querySelector(":root");
 const modeBtn = document.querySelector("#switch");
 const body = document.querySelector("body");
 
 modeBtn.addEventListener("click", function () {
+    root.classList.toggle("dark");
     body.classList.toggle("dark");
 });
