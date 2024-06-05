b69ebe2605d3bb9b7ba4bae54cbb0e218f5e94b8
K-Marple <taeshinoberu@outlook.com>
2024-06-05 06:37:35 -0700

    visits to page added

---
+++ scripts/visits.js
@@ -0,0 +1,19 @@
+// calucate and store the number of visits to the site
+// create display variable
+const displayVisits = document.querySelector(".visits");
+
+// get the value already stored if any, if not make the variable 0
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+// if first visit or display number of visits
+if (numVisits !== 0) {
+    displayVisits.textContent = numVisits;
+} else {
+    displayVisits.textContent = "Welcome to my page!";
+}
+
+// add to numVisits by one
+numVisits++;
+
+// store visits in localStorage (key=numVisits-ls) (ls=localStorage)
+localStorage.setItem("numVisits-ls", numVisits);
