0827ca595472986ee6e51818cc58f35cb459a310
K-Marple <taeshinoberu@outlook.com>
2024-06-04 17:41:15 -0700

    lazyloading set up

---
+++ lesson07/scripts/lazyload.js
@@ -0,0 +1,11 @@
+//last modified
+// set copyright year
+const year = new Date();
+const copyrightYear = year.getFullYear();
+
+document.getElementById("copyrightYear").innerHTML = copyrightYear;
+
+// last modified
+let lastMod = new Date(document.lastModified);
+
+document.getElementById("lastModified").innerHTML = lastMod;
