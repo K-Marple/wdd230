d66e828a1b2a604ef7b2cfa2c8ffbdd633f8a66d
K-Marple <taeshinoberu@outlook.com>
2024-06-19 08:58:28 -0700

    activities edits

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,19 @@
 const baseURL = "https://k-marple.github.io/wdd230/";
 const linksURL = "https://k-marple.github.io/wdd230/data/links.json";
 
+const activities = document.getElementById("activities");
+
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data);
 }
 
 getLinks();
+
+const displayLinks = (lessons) => {
+    lessons.forEach((lessonData) => {
+        let list = document.createElement("li");
+        list.textContent = `${lessonData.lesson}: `;
+    });
+}
