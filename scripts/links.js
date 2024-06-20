c034797576556b263b2a695137879a0db1d6c263
K-Marple <taeshinoberu@outlook.com>
2024-06-20 12:53:15 -0700

    finished links.js

--- scripts/links.js
+++ scripts/links.js
@@ -3,17 +3,34 @@ const linksURL = "https://k-marple.github.io/wdd230/data/links.json";
 
 const activities = document.getElementById("activities");
 
-async function getLinks() {
+async function getLinks(linksURL) {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    displayLinks(data.lessons);
 }
 
-getLinks();
-
-const displayLinks = (lessons) => {
-    lessons.forEach((lessonData) => {
+const displayLinks = (weeks) => {
+    weeks.forEach((weekData) => {
         let list = document.createElement("li");
-        list.textContent = `${lessonData.lesson}: `;
+
+        list.textContent = `${weekData.lesson}: `;
+
+
+        weekData.links.forEach((linkData, linkLength) => {
+            let link = document.createElement("a");
+
+            link.setAttribute("href", linkData.url);
+            link.textContent = linkData.title;
+
+            list.appendChild(link);
+
+            if (linkLength + 1 < weekData.links.length) {
+                link.innerHTML = `${link.innerHTML} ~`;
+            }
+        });
+
+        activities.appendChild(list);
     });
 }
+
+getLinks(linksURL);
