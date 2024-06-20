abc548aa7e47613dd1cfb16a8335f23f64743ff8
K-Marple <taeshinoberu@outlook.com>
2024-06-20 16:01:15 -0700

    directory

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,44 @@
+const directoryURL = "https://k-marple.github.io/wdd230/chamber/data/members.json";
+
+const directory = document.querySelector("#directory");
+
+async function getMemberData(directoryURL) {
+    const response = await fetch(directoryURL);
+    const data = await response.json();
+
+    console.log(data.members);
+    displayMembers(data.members);
+}
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement("section");
+        let company = document.createElement("h2");
+        let address = document.createElement("p");
+        let phone = document.createElement("p");
+        let website = document.createElement("a");
+        let logo = document.createElement("img");
+
+        company.textContent = member.name;
+        address.textContent = member.address;
+        phone.textContent = member.phone;
+
+        logo.setAttribute("src", member.image);
+        logo.setAttribute("alt", `${member.name} logo`);
+        logo.setAttribute("loading", "lazy");
+        logo.setAttribute("width", "500");
+        logo.setAttribute("height", "500");
+
+        website.setAttribute("href", member.website);
+
+        card.appendChild(company);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+        card.appendChild(logo);
+
+        directory.appendChild(card);
+    });
+}
+
+getMemberData(directoryURL);
