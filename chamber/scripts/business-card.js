6b9570c798f0bcd6d07a727b1c6fd94cf4046353
K-Marple <taeshinoberu@outlook.com>
2024-07-06 21:08:40 -0700

    dynamic business cards

--- chamber/scripts/business-card.js
+++ chamber/scripts/business-card.js
@@ -4,31 +4,57 @@ const businessCard = document.querySelector(".business-card");
 async function getBusiness(businessURL) {
     const response = await fetch(businessURL);
     const data = await response.json();
-    console.log(data.members);
+
     displayBusiness(data.members);
 }
 
-const displayBusiness = (members) => {
-    members.forEach((business) => {
-        console.log(business);
-        if (business.memberLevel == "Silver") {
-            let busiCard = document.createElement("div");
-            let busiName = document.createElement("h2");
-            let busiDesc = document.createElement("p");
+function displayBusiness(business) {
+    let businesses = [
+        business.filter(level => business.memberLevel == "Silver"),
+        business.filter(level => business.memberLevel == "Gold")
+    ]
 
-            busiName.textContent = business.name;
-            busiDesc.textContent = business.description;
+    let cards = [];
 
-            console.log(busiName);
-            console.log(busiDesc);
+    let cardNum = 0
+    while (cardNum < 3) {
+        let randomBusiness = getRandomBusiness(businesses);
+        let randomCard = getRandomBusiness(randomBusiness);
 
-            busiCard.appendChild(busiName);
-            busiCard.appendChild(busiDesc);
+        if (!cards.includes(randomCard)) {
+            cards.push(randomCard);
 
-            businessCard.appendChild(busiCard);
+            cardNum++;
         }
-    })
+    }
+}
 
+function createCard(business) {
+    let busiCard = document.createElement("div");
+    let busiName = document.createElement("h2");
+    let busiLogo = document.createElement("img");
+    let busiDesc = document.createElement("p");
+
+    busiName.textContent = business.name;
+    busiDesc.textContent = business.description;
+
+    busiLogo.setAttribute("src", business.image);
+    busiLogo.setAttribute("alt", `${business.name} logo`);
+    busiLogo.setAttribute("loading", "lazy");
+    busiLogo.setAttribute("width", "500");
+    busiLogo.setAttribute("height", "500");
+
+    busiCard.appendChild(busiLogo);
+    busiCard.appendChild(busiName);
+    busiCard.appendChild(busiDesc);
+
+    businessCard.appendChild(busiCard);
+}
+
+function getRandomBusiness(busi) {
+    const random = Math.floor(Math.random() * busi.length);
+    const randomBusi = busi[random];
+    return randomBusi;
 }
 
 getBusiness(businessURL);
