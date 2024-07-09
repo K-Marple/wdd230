0b1b0a2bc6be5fb2dc5bf74f4bcc1a3f92804f62
K-Marple <taeshinoberu@outlook.com>
2024-07-09 15:25:52 -0700

    scoots vehicles

---
+++ scoots/scripts/vehicles.js
@@ -0,0 +1,24 @@
+const vehicleURL = "https://k-marple.github.io/scoots/data/rentals.json";
+const vehicleCard = document.querySelector(".vehicles");
+
+async function getVehicleInfo(vehicleURL) {
+    const response = await fetch(vehicleURL);
+    const data = await response.json();
+    console.log(data);
+    displayVehicles(data.rentalOptions);
+}
+
+const displayVehicles = (rentals) => {
+    rentals.forEach((rental) => {
+        let type = document.createElement("div");
+
+        type.textContent = rental.type;
+        console.log(type);
+
+        let vehicle = document.createElement("p");
+
+        vehicleCard.appendChild(type);
+
+
+    });
+}
