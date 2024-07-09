871f712c3da6ae3a73de1036639334cb2248f8db
K-Marple <taeshinoberu@outlook.com>
2024-07-09 13:30:06 -0700

    scoots

---
+++ scoots/scripts/slideshow.js
@@ -0,0 +1,33 @@
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+next.addEventListener("click", () => {
+    changeSlides(1);
+})
+
+prev.addEventListener("click", () => {
+    changeSlides(-1);
+})
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function changeSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.querySelector(".slides");
+    let caption = document.querySelector("#caption");
+
+    if (n > slides.length) { slideIndex = 1 };
+    if (n < 1) { slideIndex = slides.length };
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    caption.innerHTML = slides[slideIndex - 1].alt;
+}
