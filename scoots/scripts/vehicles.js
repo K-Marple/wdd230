c64542f1a1bdc22ad34867c0cca4a5f04fd03c37
K-Marple <taeshinoberu@outlook.com>
2024-07-22 18:58:17 -0700

    rentals page

--- scoots/scripts/vehicles.js
+++ scoots/scripts/vehicles.js
@@ -4,7 +4,6 @@ const vehicleCard = document.querySelector(".tabcontent");
 async function getVehicleInfo(vehicleURL) {
     const response = await fetch(vehicleURL);
     const data = await response.json();
-    console.log(data);
     displayVehicles(data.rentalOptions);
 }
 
@@ -18,6 +17,7 @@ const displayVehicles = (rentals) => {
     typeJ.textContent = rentals[2].type;
 
     rentals.forEach((rental) => {
+        let content = document.createElement("div");
         let image = document.createElement("img");
         let vehicle = document.createElement("h4");
         let size = document.createElement("p");
@@ -27,13 +27,67 @@ const displayVehicles = (rentals) => {
         image.setAttribute("src", rental.image);
         image.setAttribute("alt", rentals.name);
         image.setAttribute("loading", "lazy");
-        image.setAttribute("")
+        image.setAttribute("width", "259");
+        image.setAttribute("height", "194");
 
-        vehicleCard.appendChild(image);
-        vehicleCard.appendChild(vehicle);
-        vehicleCard.appendChild(size);
-        vehicleCard.appendChild(other);
-        vehicleCard.appendChild(price);
+        vehicle.innerHTML = rental.name;
+        size.innerHTML = `Occupancy: ${rental.occupancy}`;
+        other.innerHTML = `Notes: ${rental.other}`;
+
+        let row1 = document.createElement("tr");
+        let reservation = document.createElement("th");
+        let walkin = document.createElement("th");
+        let row2 = document.createElement("tr");
+        let cell1 = document.createElement("td");
+        let cell2 = document.createElement("td");
+        let cell3 = document.createElement("td");
+        let cell4 = document.createElement("td");
+        let row3 = document.createElement("tr");
+        let cell5 = document.createElement("td");
+        let cell6 = document.createElement("td");
+        let cell7 = document.createElement("td");
+        let cell8 = document.createElement("td");
+
+        reservation.textContent = `Reservation`;
+        walkin.textContent = `Walk-In`;
+
+        cell1.textContent = `Half Day`;
+        cell2.textContent = `Full Day`;
+        cell3.textContent = `Half Day`;
+        cell4.textContent = `Full Day`;
+
+        cell5.textContent = rental.price;
+        cell6.textContent = rental.price;
+        cell7.textContent = rental.price;
+        cell8.textContent = rental.price;
+
+        console.log(rental.price);
+
+
+        row1.appendChild(reservation);
+        row1.appendChild(walkin);
+
+        row2.appendChild(cell1);
+        row2.appendChild(cell2);
+        row2.appendChild(cell3);
+        row2.appendChild(cell4);
+
+        row3.appendChild(cell5);
+        row3.appendChild(cell6);
+        row3.appendChild(cell7);
+        row3.appendChild(cell8);
+
+        price.appendChild(row1);
+        price.appendChild(row2);
+        price.appendChild(row3);
+
+        content.appendChild(image);
+        content.appendChild(vehicle);
+        content.appendChild(size);
+        content.appendChild(other);
+        content.appendChild(price);
+
+        vehicleCard.appendChild(content);
     });
 }
 
