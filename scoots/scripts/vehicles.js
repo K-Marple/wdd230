4d3d4666b75f624e2d4e68f57b950681e371e628
K-Marple <taeshinoberu@outlook.com>
2024-07-22 19:37:40 -0700

    json edits

--- scoots/scripts/vehicles.js
+++ scoots/scripts/vehicles.js
@@ -34,52 +34,55 @@ const displayVehicles = (rentals) => {
         size.innerHTML = `Occupancy: ${rental.occupancy}`;
         other.innerHTML = `Notes: ${rental.other}`;
 
-        let row1 = document.createElement("tr");
-        let reservation = document.createElement("th");
-        let walkin = document.createElement("th");
-        let row2 = document.createElement("tr");
-        let cell1 = document.createElement("td");
-        let cell2 = document.createElement("td");
-        let cell3 = document.createElement("td");
-        let cell4 = document.createElement("td");
-        let row3 = document.createElement("tr");
-        let cell5 = document.createElement("td");
-        let cell6 = document.createElement("td");
-        let cell7 = document.createElement("td");
-        let cell8 = document.createElement("td");
-
-        reservation.textContent = `Reservation`;
-        walkin.textContent = `Walk-In`;
-
-        cell1.textContent = `Half Day`;
-        cell2.textContent = `Full Day`;
-        cell3.textContent = `Half Day`;
-        cell4.textContent = `Full Day`;
-
-        cell5.textContent = rental.price;
-        cell6.textContent = rental.price;
-        cell7.textContent = rental.price;
-        cell8.textContent = rental.price;
-
         console.log(rental.price);
+        console.log(vehicle);
 
+        // let row1 = document.createElement("tr");
+        // let reservation = document.createElement("th");
+        // let walkin = document.createElement("th");
 
-        row1.appendChild(reservation);
-        row1.appendChild(walkin);
+        // let row2 = document.createElement("tr");
+        // let cell1 = document.createElement("td");
+        // let cell2 = document.createElement("td");
+        // let cell3 = document.createElement("td");
+        // let cell4 = document.createElement("td");
 
-        row2.appendChild(cell1);
-        row2.appendChild(cell2);
-        row2.appendChild(cell3);
-        row2.appendChild(cell4);
+        // let row3 = document.createElement("tr");
+        // let cell5 = document.createElement("td");
+        // let cell6 = document.createElement("td");
+        // let cell7 = document.createElement("td");
+        // let cell8 = document.createElement("td");
 
-        row3.appendChild(cell5);
-        row3.appendChild(cell6);
-        row3.appendChild(cell7);
-        row3.appendChild(cell8);
+        // reservation.textContent = `Reservation`;
+        // walkin.textContent = `Walk-In`;
 
-        price.appendChild(row1);
-        price.appendChild(row2);
-        price.appendChild(row3);
+        // cell1.textContent = `Half Day`;
+        // cell2.textContent = `Full Day`;
+        // cell3.textContent = `Half Day`;
+        // cell4.textContent = `Full Day`;
+
+        // cell5.textContent = rental.price.reservation[0];
+        // cell6.textContent = rental.price.reservation[1];
+        // cell7.textContent = rental.price.walkIn[0];
+        // cell8.textContent = rental.price.walkIn[1];
+
+        // row1.appendChild(reservation);
+        // row1.appendChild(walkin);
+
+        // row2.appendChild(cell1);
+        // row2.appendChild(cell2);
+        // row2.appendChild(cell3);
+        // row2.appendChild(cell4);
+
+        // row3.appendChild(cell5);
+        // row3.appendChild(cell6);
+        // row3.appendChild(cell7);
+        // row3.appendChild(cell8);
+
+        // price.appendChild(row1);
+        // price.appendChild(row2);
+        // price.appendChild(row3);
+
 
         content.appendChild(image);
         content.appendChild(vehicle);
@@ -89,6 +92,24 @@ const displayVehicles = (rentals) => {
 
         vehicleCard.appendChild(content);
     });
+
 }
+
+const btn = document.querySelector(".tablink")
+
+btn.addEventListener("click", function openTab(pageName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.background = "";
+    }
+
+    document.getElementById(pageName).style.display = "block";
+})
 
 getVehicleInfo(vehicleURL);
